f5f00a4e2f7a898a5a3041abd0fb44ec9aec4fe8
caotien <caovtien22@gmail.com>
2020-07-01 16:01:01 +0700

    Add Router

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,17 +1,33 @@
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
 
 public class Launcher extends AbstractVerticle {
     @Override
     public void start(Future future) throws Exception {
+        Router router = Router.router(vertx);
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>Hello The World Again</h1>");
+        });
         vertx
                 .createHttpServer()
-                .requestHandler(request -> {
-                    request.response().end("<h1> Hello World <h1>");
-                })
-                .listen(8080, result -> {
-                    if (result.succeeded()) future.complete();
-                    else future.fail(result.cause());
-                });
+                .requestHandler(router::accept)
+                .listen(
+                        // Retrieve the port from the configuration,
+                        // default to 8080.
+                        config().getInteger("http.port", 8080),
+                        result -> {
+                            if (result.succeeded()) {
+                                future.complete();
+                            } else {
+                                future.fail(result.cause());
+                            }
+                        }
+                );
     }
 }
